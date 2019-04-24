5697ec0bb3b1478535c18cd32b0e4e39286ca9fa
楊崇平 <god913106@gmail.com>
2019-04-24 19:02:59 +0800

    Cs193P 斯坦福P4

--- Concentration/Concentration/Model/Concentration.swift
+++ Concentration/Concentration/Model/Concentration.swift
@@ -10,51 +10,72 @@ import Foundation
 
 class Concentration {
     
-    var cards = [Card]()
+    private(set) var cards = [Card]()
     
-    var indexOfOneAndOnlyFaceUpCard: Int?
+    private var indexOfOneAndOnlyFaceUpCard: Int?{
+        get {
+            var foundIndex : Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index //first card
+                    } else {
+                        return nil // turn second card so return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set{
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
+    
+    /*
+     
+     var indexOfOneAndOnlyFaceUpCard: Int? {
+     get {
+     // look at all the cards and see if you find only one that's face up.
+     // if so, return it , else return nil.
+     }
+     set {
+     // turn all the cards face down except the card at index newValue.
+     }
+     
+     */
     
     /*
      情境：
      (1)沒有卡片面朝上，選了一張卡，那就翻過來。
      (2)兩張卡片面朝上但不同emoji，當我再選一張卡時，就預要讓前兩張卡面朝下，開始新一輪配對。
      (3)有一張卡面朝上，然後選了另一張卡，現在要試著配對，看看是否匹配的。
-
+     
      */
     func chooseCard(at index: Int) {
-        
-        
-//
-//            if cards[index].isFaceUp {
-//                cards[index].isFaceUp = false
-//            }else {
-//                cards[index].isFaceUp = true
-//            }
-        
-            if !cards[index].isMatched {
-                if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
-                    //check if cards match
-                    if cards[matchIndex].identifier == cards[index].identifier {
-                        cards[matchIndex].isMatched = true
-                        cards[index].isMatched = true
-                    }
-                    cards[index].isFaceUp = true
-                    indexOfOneAndOnlyFaceUpCard = nil
-                } else {
-                    // (2)either no cards or cards are face up
-                    for flipDownIndex in cards.indices {
-                        cards[flipDownIndex].isFaceUp = false
-                    }
-                    cards[index].isFaceUp = true
-                    indexOfOneAndOnlyFaceUpCard = index
+        // assertion，斷言，通過聲明一個斷言來確保某個必要的條件是滿足的，以便繼續執行接下來的代碼。 index = -1 minus one no in the cards
+        assert(cards.indices.contains(index), "Concentration.chooseCard(at:\(index)):chosen index no in the cards")
+        if !cards[index].isMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                //check if cards match
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
                 }
+                cards[index].isFaceUp = true
+            } else {
+                // (2)either no cards or cards are face up
+                indexOfOneAndOnlyFaceUpCard = index
             }
-        
-        
+        }
     }
     
     //生成兩張相同卡片
     init(numberOfPairsOfCards: Int) {
+        
+        assert(numberOfPairsOfCards > 0 , "Concentration.init(\(numberOfPairsOfCards)): you must have at least one pair of cards ")
+        
         for _ in 1...numberOfPairsOfCards {
             let card = Card()
             cards += [card, card]
--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -10,22 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+    private lazy var game = Concentration(numberOfPairsOfCards: numberOfPairsOfCards)
     
-    var flipCount = 0 {
+    var numberOfPairsOfCards: Int {
+        return (cardButtons.count + 1) / 2
+    }
+    
+    private(set) var flipCount = 0 {
         didSet{
             flipCountLabel.text = "Flips: \(flipCount)"
         }
     }
     
-    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet private weak var flipCountLabel: UILabel!
     
     
-    @IBOutlet var cardButtons: [UIButton]!
+    @IBOutlet private var cardButtons: [UIButton]!
     
     
     
-    @IBAction func touchCard(_ sender: UIButton) {
+    @IBAction private func touchCard(_ sender: UIButton) {
         
         flipCount += 1
         
@@ -44,7 +48,7 @@ class ViewController: UIViewController {
        
     }
   
-    func updateViewFromModel() {
+    private func updateViewFromModel() {
         
         // 0..<cardButtons.count = cardButtons.indices  數組所有索引組成的可數區間
         for index in cardButtons.indices {
@@ -62,17 +66,19 @@ class ViewController: UIViewController {
         
     }
     
-    var emojiChoices = ["👻", "🎃", "🐶", "🐯", "🐳", "🍔", "🍱", "🍧", "🔥"]
+    private var emojiChoices = ["👻", "🎃", "🐶", "🐯", "🐳", "🍔", "🍱", "🍧", "🔥"]
     
-    var emoji = [Int : String]()
+    private var emoji = [Int : String]()
     
-    func emoji(for card: Card) -> String {
+    private func emoji(for card: Card) -> String {
         
         if emoji[card.identifier] == nil, emojiChoices.count > 0 {
  
-                let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+//                let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+            
+                
                 // 隨機到哪個 就remove掉emojiChoices的那個 再賦值給 var emoji = [Int : String]()
-                emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+                emoji[card.identifier] = emojiChoices.remove(at: emojiChoices.count.arc4random)
             
         }
         
@@ -82,3 +88,15 @@ class ViewController: UIViewController {
  
 }
 
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+          return 0
+        }
+        
+    }
+}
